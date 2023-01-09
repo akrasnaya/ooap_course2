9bd479bd39cc00b10986cf59e5432fce9c8162c5
akrasnaya <59962274+akrasnaya@users.noreply.github.com>
2023-01-09 17:55:02 +0300

    Update task12.py

--- task12.py
+++ task12.py
@@ -1,16 +1,8 @@
-class General():
-    def __init__(self):
-        pass
-    
-    def void(self):
-        pass
-
 class Any(General):
-    pass
 
-class None(General, Any):
-    def __init__(self):
-        super(None, self).__init__()
-    
-    def void(self):
-        super(None, self).void()
+    @classmethod
+    def assignment_attempt(cls, target, source):
+        if isinstance(target, cls) and isinstance(source, cls):
+            target.value = source.value
+            return target
+        return Void
